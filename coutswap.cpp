3e36ca43620fa09c6f7a16463f3d5028fedb5c6b
Parth Bedmutha <bedmuthaparth@gmail.com>
2025-03-15 20:01:21 +0530

    coutswap.cpp
    swaping of 4 numbers using cout

---
+++ coutswap.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d;
+	int swap;
+	
+	cout<<"enter value of a:";
+	cin>>a;
+	cout<<"enter value of b:";
+	cin>>b;
+	cout<<"enter value of c:";
+	cin>>c;
+	cout<<"enter value of d:";
+	cin>>d;
+	
+	cout<<"after swaping values:"<<d<<c<<b<<a;
+	
+	return 0;
+}
