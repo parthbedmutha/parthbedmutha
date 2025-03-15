fdfb94c1503815085167df757328cabc9acf9190
Parth Bedmutha <bedmuthaparth@gmail.com>
2025-03-15 20:02:28 +0530

    alternateswap.cpp
    alternate swaping program of 4 numbers

---
+++ alternateswap.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=8,b=4,c=2,d=3;
+	int swap;
+	int swap2;
+
+	
+	swap=a;
+	a=d;
+	swap2=b;
+	swap2=c;
+	swap=d;
+	
+	cout<<"after swaping values:"<<a<<b<<c<<d;
+	
+	return 0;
+	
+}
