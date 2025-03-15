f08c8e7d659968af53ebc3bb57d9ec6b95fba40d
Parth Bedmutha <bedmuthaparth@gmail.com>
2025-03-15 20:03:37 +0530

    simpleifelse.cpp
    simple cpp program of if else

---
+++ simpleifelse.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	int a=10;
+	int b=20;
+	
+	if(a==b || b==a)
+	{ 
+	cout<<"true";
+	}
+	else 
+	{
+		cout<<"false";
+		
+	}	
+	
+	return 0;
+	
+}
